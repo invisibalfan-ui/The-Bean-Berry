f4667133356aaf3f1f1ebe720fe2c964010e1da6
invisibalfan-ui <invisibalfan@gmail.com>
2026-04-08 08:52:52 -0400

    up

--- admin-dashboard/vite.config.js
+++ admin-dashboard/vite.config.js
@@ -3,24 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // ensures assets are relative for production
   build: {
-    outDir: 'dist'
-  },
-  define: {
-    'process.env': process.env
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      // Allows JSX in .js files during development
-      loader: {
-        '.js': 'jsx',
-      },
-    },
-  },
-  esbuild: {
-    // Allows JSX in .js files during the build process
-    loader: 'jsx',
-    include: /src\/.*\.js$/, // Target only your source files
-    exclude: [],
+    outDir: 'dist',
+    emptyOutDir: true,
   },
 })
